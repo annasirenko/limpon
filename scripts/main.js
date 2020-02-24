34073df5275ef74cdba9acb4a8b4afac52fc33e0
Anna <otherjam@gmail.com>
2020-02-24 17:04:18 +0200

    add js fadein for team socials

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $(".team__user-button-ricardo").click(function() {
+    $(".team__user-button-ricardo").hide();
+    $(".team__socials-ricardo").fadeIn();
+  });
+  $(".team__user-button-thomas").click(function() {
+    $(".team__user-button-thomas").hide();
+    $(".team__socials-thomas").fadeIn();
+  });
+  $(".team__user-button-alberto").click(function() {
+    $(".team__user-button-alberto").hide();
+    $(".team__socials-alberto").fadeIn();
+  });
+});
